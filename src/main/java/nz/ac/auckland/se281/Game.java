fa0cb19b7c5dda897353d70f14123d6980b86a71
jkan172 <jkan172@auckalnduni.ac.nz>
2024-05-07 22:24:44 +1200

    I have changed the format of asking for the input and checking if the input is valid

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,14 +6,12 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
 
-  String playerName;
+  private String playerName;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     playerName = options[0];
     MessageCli.WELCOME_PLAYER.printMessage(playerName);
-
-
   }
 
   public void play() {
@@ -22,15 +20,25 @@ public class Game {
     rounds++;
 
     MessageCli.ASK_INPUT.printMessage();
-
     String input = Utils.scanner.nextLine();
 
+    boolean validInput = false;
+
     if ((Integer.parseInt(input) < 0) || (Integer.parseInt(input) > 5)) {
-      MessageCli.INVALID_INPUT.printMessage();
+      while (!validInput) {
+        MessageCli.INVALID_INPUT.printMessage();
+
+        MessageCli.ASK_INPUT.printMessage();
+        input = Utils.scanner.nextLine();
+
+        if ((Integer.parseInt(input) >= 0) && (Integer.parseInt(input) <= 5)) {
+          validInput = true;
+          break;
+        }
+      }
     }
 
     MessageCli.PRINT_INFO_HAND.printMessage(playerName, input);
-
   }
 
   public void endGame() {}
