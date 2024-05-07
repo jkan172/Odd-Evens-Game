2293d70bfe5a3e57457e136b6f2ac156da172bb9
jkan172 <jkan172@auckalnduni.ac.nz>
2024-05-08 04:39:17 +1200

    I have added some codes for medium difficulty constraints

--- src/main/java/nz/ac/auckland/se281/Medium.java
+++ src/main/java/nz/ac/auckland/se281/Medium.java
@@ -1,7 +1,20 @@
 package nz.ac.auckland.se281;
 
 public class Medium implements AI {
+
+  @Override
   public int play() {
-    return 0;
+
+    Game game = new Game();
+
+    if (game.getCurrentRound() <= 3) {
+      RandomStrategy randomStrategy = new RandomStrategy();
+      int num = randomStrategy.setStrategy();
+      return num;
+    } else {
+      TopStrategy topStrategy = new TopStrategy();
+      int num2 = topStrategy.setStrategy();
+      return num2;
+    }
   }
 }
