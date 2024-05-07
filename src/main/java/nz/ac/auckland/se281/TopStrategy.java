fa10b4e1c33737f6566cfba677debcb0e104166f
jkan172 <jkan172@auckalnduni.ac.nz>
2024-05-08 04:36:36 +1200

    I have added the codes for Top strategy constraints

--- src/main/java/nz/ac/auckland/se281/TopStrategy.java
+++ src/main/java/nz/ac/auckland/se281/TopStrategy.java
@@ -1,7 +1,45 @@
 package nz.ac.auckland.se281;
 
+import nz.ac.auckland.se281.Main.Choice;
+
 public class TopStrategy implements Strategy {
+  private int oddCount;
+  private int evenCount;
+
+  @Override
   public int setStrategy() {
-    return 0;
+
+    Game game = new Game();
+    if (game.getCurrentChoice() == Choice.ODD) {
+
+      if (Utils.isOdd(game.getCurrentInput())) {
+        oddCount++;
+      } else {
+        evenCount++;
+      }
+
+      if (oddCount > evenCount) {
+        return Utils.getRandomOddNumber();
+      } else if (evenCount > oddCount) {
+        return Utils.getRandomEvenNumber();
+      } else {
+        return Utils.getRandomNumberRange(0, 5);
+      }
+    } else {
+
+      if (Utils.isEven(game.getCurrentInput())) {
+        evenCount++;
+      } else {
+        oddCount++;
+      }
+
+      if (evenCount > oddCount) {
+        return Utils.getRandomOddNumber();
+      } else if (oddCount > evenCount) {
+        return Utils.getRandomEvenNumber();
+      } else {
+        return Utils.getRandomNumberRange(0, 5);
+      }
+    }
   }
 }
